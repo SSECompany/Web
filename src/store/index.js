5af3f4cc241f1a2ed381e9dcd605faad8f4e1309
huudat <huudat2903@gmail.com>
2026-05-20 09:58:52 +0700

    refactor: remove legacy pharmacy POS module and related state management files

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import kho from "./../pages/kho/store/kho";
-import orders from "./../pages/pharmacy/store/order";
-import returnOrders from "./../pages/pharmacy/store/returnOrder";
 import claimsReducer from "./reducers/claimsSlice";
 import loadingReducer from "./reducers/loadingSlice";
 //store
@@ -9,8 +7,6 @@ export const store = configureStore({
   reducer: {
     loadingReducer,
     claimsReducer,
-    orders: orders,
-    returnOrders: returnOrders,
     kho: kho,
   },
   middleware: (getDefaultMiddleware) =>
